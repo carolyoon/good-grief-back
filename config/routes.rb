b6322b6935885ffaa7bcd9f3d0fa39d969e1ddfa
Quynh Nguyen <qnguyenx3@gmail.com>
2017-12-02 16:43:52 -0800

    Define routes for journal entries

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+
+  get '/journal_entries'
+  post '/journal_entries'
+
   resources :goals, only: [:new, :create, :update, :destroy]
 
   resources :users, only: [:create, :show, :update]
+
+  resources :journal_entries, only: [:index, :create]
 end
