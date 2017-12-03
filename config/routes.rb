720ee5f8d82e6b3c887774c3dcac13bc26d847b3
Caroline <carolyoon731@gmail.com>
2017-12-02 19:44:51 -0800

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :goals, only: [:new, :create, :update, :destroy]
+  namespace :api do
+    resources :goals, only: [:new, :create, :update, :destroy]
 
-  resources :users, only: [:create, :show, :update]
+    resources :users, only: [:create, :show, :update]
+  end    
 end
