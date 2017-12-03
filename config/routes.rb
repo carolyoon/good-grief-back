c1d833938681936ac0c93593c40aec2162eed89c
Quynh Nguyen <qnguyenx3@gmail.com>
2017-12-03 15:03:09 -0800

    Nest goals and journal entries routes under users

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  namespace :api do
-    resources :goals, only: [:new, :create, :update, :destroy]
 
-    resources :users, only: [:create, :show, :update]
-    
-    resources :journal_entries, only: [:index, :create]
+  namespace :api do
+    resources :users do
+      resources :goals, only: [:index, :create, :update, :destroy]
+
+      resources :journal_entries, only: [:index, :create]
+    end
+      resources :users, only: [:create, :show, :update]
   end
 end
