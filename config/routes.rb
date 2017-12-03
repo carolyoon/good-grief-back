d4e37a7eaa5a8a0e22f39d793ec15c38dca22965
Quynh Nguyen <qnguyenx3@gmail.com>
2017-12-02 17:06:28 -0800

    Remove get and post paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
 
-  get '/journal_entries'
-  post '/journal_entries'
-
   resources :goals, only: [:new, :create, :update, :destroy]
 
   resources :users, only: [:create, :show, :update]
