ae955a209d77fba79f6cd716c634caf31db196cd
Quynh Nguyen <qnguyenx3@gmail.com>
2017-12-02 18:27:37 -0800

    Remove new path for goals in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :goals, only: [:new, :create, :update, :destroy]
+  resources :goals, only: [:create, :update, :destroy]
 
   resources :users, only: [:create, :show, :update]
 
