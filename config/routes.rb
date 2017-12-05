9d7eb406631bd18e799cb06fcbfaa026b28e7942
AllyJo <gatorally245@gmail.com>
2017-12-04 21:01:27 -0800

    create sessions controller and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   post 'user_token' => 'user_token#create'
   namespace :api do
+    resources :sessions, only: [:create, :destroy]
     resources :users do
       resources :goals, only: [:index, :create, :update, :destroy]
 
